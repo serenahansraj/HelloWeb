96df4cb799413272556dfa014cf358b4596dd6d3
serenahansraj <shansraj@smith.edu>
2016-10-25 15:09:37 -0400

    HelloWeb
    This is a test

---
+++ src/java/org/mypackage/hello/NameHandler.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.hello;
+
+/**
+ *
+ * @author serenahansraj
+ */
+public class NameHandler {
+    String name;
+
+    public NameHandler() {
+        name=null;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
